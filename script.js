509fd08fe55e1591e47640c923ee45f5031e3a07
Ramkrishn <ram706860@gmail.com>
2022-11-03 22:58:21 +0530

    some basic changes

--- script.js
+++ script.js
@@ -10,4 +10,8 @@ setInterval(() => {
     hour.style.transform = `rotate(${hrotation}deg)`;
     minute.style.transform = `rotate(${mrotation}deg)`;
     second.style.transform = `rotate(${srotation}deg)`;
+
+    htime < 10 ? hr.innerText = "0" + htime : hr.innerText = htime;
+    mtime < 10 ? min.innerText = "0" + mtime : min.innerText = mtime;
+    stime < 10 ? sec.innerText = "0" + stime : sec.innerText = stime;
 }, 1000);
